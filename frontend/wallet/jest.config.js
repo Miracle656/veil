1d448ed33c26168d452e31625de0ead8d4341e0e
samad13 <yusufabdulsamad93@gmail.com>
2026-04-25 21:47:18 +0100

    add unit test

---
+++ frontend/wallet/jest.config.js
@@ -0,0 +1,24 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        jsx: 'react',
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true,
+      },
+    }],
+  },
+  collectCoverageFrom: [
+    'lib/**/*.ts',
+    '!lib/**/*.d.ts',
+    '!lib/__tests__/**',
+  ],
+}
+
+module.exports = config
