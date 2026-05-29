bf12ae9cf55ad5a7cf0293e534b7c90d36264b0c
Tee <knapstoremail@gmail.com>
2026-05-29 21:09:27 +0100

    test: import Address and Ledger test utilities and IntoVal trait for unit tests

--- contracts/invisible_wallet/src/lib.rs
+++ contracts/invisible_wallet/src/lib.rs
@@ -427,7 +427,8 @@ impl InvisibleWallet {
 #[cfg(test)]
 mod test {
     use super::*;
-    use soroban_sdk::{Env, Bytes, BytesN, symbol_short, Map};
+    use soroban_sdk::{Env, Bytes, BytesN, symbol_short, Map, IntoVal};
+    use soroban_sdk::testutils::{Address as _, Ledger as _};
     use sha2::{Sha256, Digest};
     use p256::ecdsa::{SigningKey, Signature as P256Sig, signature::hazmat::PrehashSigner};
 
